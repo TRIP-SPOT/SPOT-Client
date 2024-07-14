c106b1fd962a53b4fb210871a4bca25cc68083e3
d0422 <rlfehd2013@naver.com>
2024-07-15 00:53:32 +0900

    config: pretendard 글자체 추가

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -3,7 +3,13 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pm: ['Pretendard-Medium'],
+        pb: ['Pretendard-Bold'],
+        pl: ['Pretendard-Light'],
+      },
+    },
   },
   plugins: [],
 };
