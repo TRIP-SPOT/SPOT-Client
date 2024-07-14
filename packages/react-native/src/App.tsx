f31250e85eb8fb08b7488b94687e39c62c6dfab0
d0422 <rlfehd2013@naver.com>
2024-07-14 23:06:25 +0900

    chore: 디자인 시스템 의존성 추가

--- packages/react-native/src/App.tsx
+++ packages/react-native/src/App.tsx
@@ -5,14 +5,15 @@
  * @format
  */
 
-import { useState } from 'react';
+import { Button } from 'design-system';
+import React, { useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 function App(): React.JSX.Element {
   const [state, setState] = useState(1);
 
   return (
-    <View className="flex-1 items-center justify-center bg-blue-300">
+    <View className="flex-1 items-center justify-center">
       <Text className="text-red-300">
         Open up App.js to start working on your app!
         {state}
@@ -20,6 +21,10 @@ function App(): React.JSX.Element {
       <TouchableOpacity onPress={() => setState((prev) => prev + 1)}>
         <Text> Click Me!</Text>
       </TouchableOpacity>
+      <Button
+        text="버튼!!!!"
+        onPress={() => setState((prev) => prev + 1)}
+      ></Button>
     </View>
   );
 }
--- packages/react-native/tailwind.config.js
+++ packages/react-native/tailwind.config.js
@@ -2,4 +2,8 @@ const { tailwindConfig } = require('tailwind-config');
 
 module.exports = {
   ...tailwindConfig,
+  content: [
+    ...tailwindConfig.content,
+    '../design-system/src/**/*.{js,jsx,ts,tsx}',
+  ],
 };
