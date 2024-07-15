0f2b38a6d32e1445b90d73edfb12e90f8e123ca7
HBSPS <wiyeong79@naver.com>
2024-07-16 00:12:16 +0900

    feat: 검색바 아이콘 추가

--- packages/react-native/src/components/common/SearchBar.tsx
+++ packages/react-native/src/components/common/SearchBar.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { TextInput } from 'react-native';
+import { Alert, TextInput, TouchableOpacity, View } from 'react-native';
+import Search from '../../assets/Search';
 
 interface SearchBarProps {
   placeholder: string;
@@ -8,14 +9,31 @@ interface SearchBarProps {
 export default function SearchBar({ placeholder }: SearchBarProps) {
   const [searchKeyword, setSearchKeyword] = useState('');
 
+  const search = () => {
+    if (!searchKeyword) return;
+
+    Alert.alert(searchKeyword);
+  };
+
   return (
-    <TextInput
-      value={searchKeyword}
-      onChangeText={(newKeyword) => setSearchKeyword(newKeyword)}
-      placeholder={placeholder}
-      placeholderTextColor="#0F0F0F"
-      // FIXME: 공통 폰트 디자인 적용: text-body1 text-spot-black
-      className="rounded-md p-4 opacity-60 bg-white border text-base text-[#0F0F0F]"
-    />
+    <View className="relative items-center justify-center">
+      <TextInput
+        value={searchKeyword}
+        onChangeText={(newKeyword) => setSearchKeyword(newKeyword)}
+        placeholder={placeholder}
+        placeholderTextColor="#0F0F0F"
+        // FIXME: 공통 폰트 디자인 적용: text-body1 text-spot-black
+        className="w-full rounded-md p-4 opacity-60 bg-white border text-base text-spot-black"
+        onSubmitEditing={search}
+      />
+      <TouchableOpacity
+        className="absolute right-2.5 w-10 h-10 items-center justify-center rounded-full"
+        style={{ backgroundColor: 'rgba(15,15,15,0.15)' }}
+        onPress={search}
+        disabled={!searchKeyword}
+      >
+        <Search color="#0F0F0F" />
+      </TouchableOpacity>
+    </View>
   );
 }
