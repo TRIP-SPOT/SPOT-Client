666c634c376aac276af8db27dd29c4472a33b15e
HBSPS <wiyeong79@naver.com>
2024-07-17 20:38:24 +0900

    feat: 카메라 촬영 기능 추가

--- packages/react-native/src/pages/CameraPage.tsx
+++ packages/react-native/src/pages/CameraPage.tsx
@@ -1,19 +1,81 @@
-import { StyleSheet } from 'react-native';
-import { Camera } from 'react-native-vision-camera';
+import { useRef, useState } from 'react';
+import { Alert, Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Font } from 'design-system';
+import { Camera, PhotoFile } from 'react-native-vision-camera';
 import useCamera from '@/hooks/useCamera';
+import DownloadIcon from '@/assets/DownloadIcon';
 
 export default function CameraPage() {
+  const camera = useRef<Camera>(null);
   const { device, hasPermission } = useCamera();
+  const [photo, setPhoto] = useState<PhotoFile | null>(null);
+
+  const takePhoto = async () => {
+    if (!camera.current) return;
+
+    const newPhoto = await camera.current.takePhoto();
+    setPhoto(newPhoto);
+  };
+
+  const savePhoto = () => {
+    if (!photo) return;
+
+    Alert.alert('저장됨');
+  };
+
+  const resetPhoto = () => {
+    setPhoto(null);
+  };
 
   if (!device || !hasPermission) return null;
 
   return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={device}
-      isActive
-      photo
-      audio={false}
-    />
+    <View className="flex-1 items-center justify-center">
+      {photo && (
+        <>
+          <Image source={{ uri: photo.path }} style={StyleSheet.absoluteFill} />
+          <View className="absolute bottom-14 flex-row items-center justify-between w-full px-8">
+            <View className="w-20" />
+            <View className="w-20">
+              <TouchableOpacity
+                onPress={savePhoto}
+                className="items-center justify-center rounded-full bg-SPOT-white w-20 h-20"
+              >
+                <DownloadIcon width={60} height={60} />
+              </TouchableOpacity>
+            </View>
+            <View className="w-20">
+              <TouchableOpacity
+                onPress={resetPhoto}
+                className="bg-SPOT-white p-3 rounded-lg items-center justify-center"
+              >
+                <Font.Bold type="body1" color="black">
+                  다시찍기
+                </Font.Bold>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </>
+      )}
+      {!photo && (
+        <>
+          <Camera
+            ref={camera}
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive
+            photo
+            audio={false}
+          />
+          <TouchableOpacity
+            onPress={takePhoto}
+            className="absolute bottom-24 items-center justify-center"
+          >
+            <View className="absolute bg-white w-[80px] h-[80px] rounded-full" />
+            <View className="absolute bg-white w-[72px] h-[72px] rounded-full border-[3px] border-SPOT-black" />
+          </TouchableOpacity>
+        </>
+      )}
+    </View>
   );
 }
--- packages/react-native/src/routes/StackNavigator.tsx
+++ packages/react-native/src/routes/StackNavigator.tsx
@@ -1,5 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from '@pages/Login/Login';
+import { Text, View } from 'react-native';
 import TabScreens from '@pages/TabScreens';
 import CameraPage from '@/pages/CameraPage';
 import SignupStackNavigator from './SignupStackNavigator';
@@ -14,6 +15,16 @@ export type RootStackParamList = {
   Home: undefined;
 };
 
+function CameraHeader() {
+  return (
+    <View className="flex-1 bg-red-300" style={{ height: 80 }}>
+      <Text className="text-white">test</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator<RootStackParamList>();
+
 export default function StackNavigator() {
   return (
     <Stack.Navigator
@@ -24,7 +35,11 @@ export default function StackNavigator() {
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Landing" component={TabScreens} />
       <Stack.Screen name="Signup" component={SignupStackNavigator} />
-      <Stack.Screen name="Camera" component={CameraPage} />
+      <Stack.Screen
+        name="Camera"
+        options={{ header: CameraHeader }}
+        component={CameraPage}
+      />
     </Stack.Navigator>
   );
 }
