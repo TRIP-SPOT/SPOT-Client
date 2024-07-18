b3d0d099f06b5dc844ee8bedd81d4447e7f783ae
d0422 <rlfehd2013@naver.com>
2024-07-18 22:16:13 +0900

    feat: body3 tailwind config 추가

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -15,10 +15,11 @@ module.exports = {
         'SPOT-white': '#FFFFFF',
       },
       fontSize: {
-        body1: ['16px', '24px'],
         title1: ['22px', '30px'],
         mainTitle: ['26px', '30px'],
+        body1: ['16px', '24px'],
         body2: ['14px', '18px'],
+        body3: ['12px', '16px'],
       },
     },
   },
