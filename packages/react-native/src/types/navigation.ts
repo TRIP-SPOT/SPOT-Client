0296f155e58faef6c02634f488244b67cd610397
d0422 <rlfehd2013@naver.com>
2024-07-19 00:58:05 +0900

    type: SignupRouteProps 유틸타입 추가

--- packages/react-native/src/types/navigation.ts
+++ packages/react-native/src/types/navigation.ts
@@ -1,9 +1,15 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '@/routes/StackNavigator';
 import { SignupStackParamList } from '@/routes/SignupStackNavigator';
+import { RouteProp } from '@react-navigation/native';
 
 export type ScreenNavigationProp<T extends keyof RootStackParamList> =
   StackNavigationProp<RootStackParamList, T>;
 
 export type SignupStackNavigation<T extends keyof SignupStackParamList> =
   StackNavigationProp<SignupStackParamList, T>;
+
+export type SignupRouteProps<T extends keyof SignupStackParamList> = RouteProp<
+  SignupStackParamList,
+  T
+>;
