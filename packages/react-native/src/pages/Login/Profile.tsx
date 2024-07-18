489e915fc4f428e116af74337a79099f03e1bdd1
d0422 <rlfehd2013@naver.com>
2024-07-19 01:02:21 +0900

    feat: NicknameProfile 페이지 연결 처리

--- packages/react-native/src/pages/Login/Profile.tsx
+++ packages/react-native/src/pages/Login/Profile.tsx
@@ -1,12 +1,13 @@
 import SelectProfile from '@/assets/SelectProfile';
-import { Font } from 'design-system';
+import { Button, Font } from 'design-system';
 import { Image, View } from 'react-native';
 import Header from '@/components/signup/Header';
 import Overlay from '@/components/signup/Overlay';
-import { SignupStackNavigation } from '@/types/navigation';
+import { SignupRouteProps, SignupStackNavigation } from '@/types/navigation';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import useGallery from '@/hooks/useGallery';
 import { useState } from 'react';
+import { useRoute } from '@react-navigation/native';
 
 interface ProfileProps {
   navigation: SignupStackNavigation<'Signup/Profile'>;
@@ -15,6 +16,8 @@ interface ProfileProps {
 export default function Profile({ navigation }: ProfileProps) {
   const { getPhoto } = useGallery();
   const [photoUri, setPhotoUri] = useState('');
+  const route = useRoute<SignupRouteProps<'Signup/Profile'>>();
+  const nickname = route.params.nickname;
 
   const handlePhotoGet = async () => {
     const photo = await getPhoto();
@@ -24,37 +27,58 @@ export default function Profile({ navigation }: ProfileProps) {
     setPhotoUri(photo || '');
   };
 
+  const handleNext = () => {
+    if (!photoUri) return;
+
+    navigation.navigate('Main');
+  };
+
   return (
     <Overlay>
-      <Header
-        onBack={() => navigation.goBack()}
-        onCancel={() => navigation.navigate('Main')}
-      />
-      <View className="flex w-full mt-[30px]">
-        <Font type="mainTitle" color="white">
-          사용할 프로필 사진을
-        </Font>
-        <Font type="mainTitle" color="white">
-          설정하세요
-        </Font>
-      </View>
-      <View className="w-full mt-[60px] justify-center items-center">
-        <TouchableOpacity onPress={handlePhotoGet}>
-          {photoUri ? (
-            <Image
-              className="w-[140px] h-[140px] rounded-full bg-SPOT-white"
-              source={{ uri: photoUri }}
-              resizeMode="contain"
-            />
-          ) : (
-            <SelectProfile />
-          )}
-        </TouchableOpacity>
-        <TouchableOpacity className="mt-[30px]">
-          <Font.Bold type="body2" color="white" underline>
-            닉네임으로 프로필 사진 설정하기
+      <View className="w-full h-full justify-between">
+        <View>
+          <Header
+            onBack={() => navigation.goBack()}
+            onCancel={() => navigation.navigate('Main')}
+          />
+
+          <View className="flex w-full mt-[30px]">
+            <Font type="mainTitle" color="white">
+              사용할 프로필 사진을
+            </Font>
+            <Font type="mainTitle" color="white">
+              설정하세요
+            </Font>
+          </View>
+          <View className="w-full mt-[60px] justify-center items-center">
+            <TouchableOpacity onPress={handlePhotoGet}>
+              {photoUri ? (
+                <Image
+                  className="w-[140px] h-[140px] rounded-full bg-SPOT-white"
+                  source={{ uri: photoUri }}
+                  resizeMode="contain"
+                />
+              ) : (
+                <SelectProfile />
+              )}
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="mt-[30px]"
+              onPress={() =>
+                navigation.navigate('Signup/NicknameProfile', { nickname })
+              }
+            >
+              <Font.Bold type="body2" color="white" underline>
+                닉네임으로 프로필 사진 설정하기
+              </Font.Bold>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Button onPress={handleNext} disabled={photoUri === ''}>
+          <Font.Bold type="body2" color="white">
+            다음
           </Font.Bold>
-        </TouchableOpacity>
+        </Button>
       </View>
     </Overlay>
   );
