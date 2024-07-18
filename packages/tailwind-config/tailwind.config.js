3048b98e85e23afd673cb399322ed7283a1292ed
d0422 <rlfehd2013@naver.com>
2024-07-18 22:26:05 +0900

    feat: tailwind config 색상 추가

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
         'SPOT-red': '#FF1919',
         'SPOT-black': '#0F0F0F',
         'SPOT-white': '#FFFFFF',
+        'Button-red': '#751010',
+        'Button-gray': '#4C4C4C',
+        'Permission-green': '#00FF75',
       },
       fontSize: {
         title1: ['22px', '30px'],
