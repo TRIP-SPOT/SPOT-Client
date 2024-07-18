ac059d412815264c24c3b2aa770153e9852b4acd
d0422 <rlfehd2013@naver.com>
2024-07-19 00:18:30 +0900

    feat: 프로필 사진 설정 기능 추가

--- packages/react-native/src/pages/Login/Profile.tsx
+++ packages/react-native/src/pages/Login/Profile.tsx
@@ -1,14 +1,29 @@
+import SelectProfile from '@/assets/SelectProfile';
 import { Font } from 'design-system';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 import Header from '@/components/signup/Header';
 import Overlay from '@/components/signup/Overlay';
 import { SignupStackNavigation } from '@/types/navigation';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import useGallery from '@/hooks/useGallery';
+import { useState } from 'react';
 
 interface ProfileProps {
   navigation: SignupStackNavigation<'Signup/Profile'>;
 }
 
 export default function Profile({ navigation }: ProfileProps) {
+  const { getPhoto } = useGallery();
+  const [photoUri, setPhotoUri] = useState('');
+
+  const handlePhotoGet = async () => {
+    const photo = await getPhoto();
+
+    if (photoUri && !photo) return;
+
+    setPhotoUri(photo || '');
+  };
+
   return (
     <Overlay>
       <Header
@@ -22,6 +37,24 @@ export default function Profile({ navigation }: ProfileProps) {
         <Font type="mainTitle" color="white">
           설정하세요
         </Font>
+      </View>
+      <View className="w-full mt-[60px] justify-center items-center">
+        <TouchableOpacity onPress={handlePhotoGet}>
+          {photoUri ? (
+            <Image
+              className="w-[140px] h-[140px] rounded-full bg-SPOT-white"
+              source={{ uri: photoUri }}
+              resizeMode="contain"
+            />
+          ) : (
+            <SelectProfile />
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity className="mt-[30px]">
+          <Font.Bold type="body2" color="white" underline>
+            닉네임으로 프로필 사진 설정하기
+          </Font.Bold>
+        </TouchableOpacity>
       </View>
     </Overlay>
   );
