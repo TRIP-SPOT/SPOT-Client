59aecd878d2aebbeabffed3d12b4fb6b2447452d
d0422 <rlfehd2013@naver.com>
2024-07-20 00:12:33 +0900

    chore: ColorSlider 불필요한 스타일링 제거

--- packages/react-native/src/components/signup/nicknameProfile/ColorSlider.tsx
+++ packages/react-native/src/components/signup/nicknameProfile/ColorSlider.tsx
@@ -11,18 +11,11 @@ interface ColorSliderProps {
 
 export default function ColorSlider({ baseColor, onChange }: ColorSliderProps) {
   return (
-    <ColorPicker
-      style={{
-        flex: 1,
-      }}
-      value={baseColor}
-      onChange={onChange}
-    >
+    <ColorPicker value={baseColor} onChange={onChange}>
       <SaturationSlider
         reverse
         style={{
           borderRadius: 50,
-          height: 36,
         }}
       />
     </ColorPicker>
