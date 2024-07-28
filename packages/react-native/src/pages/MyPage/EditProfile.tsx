2095e969936eb280fdeadf75934e7f8ec46b47f6
HBSPS <wiyeong79@naver.com>
2024-07-28 23:39:48 +0900

    feat: 프로필 수정 페이지 레이아웃 추가

--- packages/react-native/src/pages/MyPage/EditProfile.tsx
+++ packages/react-native/src/pages/MyPage/EditProfile.tsx
@@ -1,15 +1,63 @@
-import { Font } from 'design-system';
-import { View } from 'react-native';
+import { useState } from 'react';
+import { Alert, TouchableOpacity, View } from 'react-native';
+import { Button, Font, TextField } from 'design-system';
 import BackGroundGradient from '@/layouts/BackGroundGradient';
+import SelectProfile from '@/assets/SelectProfile';
 
 export default function EditProfile() {
+  // FIXME: 실제 닉네임으로 변경
+  const [nickName, setNickName] = useState('');
+
   return (
-    <BackGroundGradient>
-      <View className="p-4 pt-14">
-        <Font color="white" type="title1">
-          프로필 수정
-        </Font>
+    <>
+      <BackGroundGradient>
+        <View className="p-4 pt-14">
+          <View className="flex gap-10">
+            <View className="flex items-center justify-center pt-5">
+              <SelectProfile />
+            </View>
+            <View>
+              <View>
+                <Font type="body2" color="white">
+                  닉네임
+                </Font>
+              </View>
+              <View className="mt-2">
+                <TextField
+                  placeholder="닉네임을 입력하세요."
+                  value={nickName}
+                  onChange={(newNickName) => setNickName(newNickName)}
+                />
+              </View>
+            </View>
+            <View className="flex items-center gap-2">
+              {/* FIXME: 닉네임으로 프로필 사진 변경 기능 추가 */}
+              <TouchableOpacity onPress={() => Alert.alert('닉네임으로 설정')}>
+                <Font.Bold type="body3" color="white" underline>
+                  닉네임으로 프로필 사진 설정하기
+                </Font.Bold>
+              </TouchableOpacity>
+              {/* FIXME: 로그아웃 기능 추가 */}
+              <TouchableOpacity
+                onPress={() => Alert.alert('로그아웃')}
+                className="opacity-50"
+              >
+                <Font.Bold type="body3" color="white" underline>
+                  로그아웃 하기
+                </Font.Bold>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </BackGroundGradient>
+
+      <View className="bottom-16">
+        <Button onPress={() => Alert.alert('변경')}>
+          <Font.Bold type="title1" color="white">
+            완료
+          </Font.Bold>
+        </Button>
       </View>
-    </BackGroundGradient>
+    </>
   );
 }
