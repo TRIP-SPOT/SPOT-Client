562a3855c18fd7e32f652b70a1a0709ef7b6ab54
d0422 <rlfehd2013@naver.com>
2024-07-31 00:10:26 +0900

    chore: add @gorhom/bottom-sheet

--- packages/react-native/src/App.tsx
+++ packages/react-native/src/App.tsx
@@ -1,14 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from '@routes/StackNavigator';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const queryClient = new QueryClient();
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </GestureHandlerRootView>
     </QueryClientProvider>
   );
 }
