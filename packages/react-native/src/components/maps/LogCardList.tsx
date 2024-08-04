910f61c44087032bdec782af7733128579bbb1ff
d0422 <rlfehd2013@naver.com>
2024-08-04 22:42:41 +0900

    feat: LogCard, LogCardList컴포넌트 추가

---
+++ packages/react-native/src/components/maps/LogCardList.tsx
@@ -0,0 +1,59 @@
+import { View } from 'react-native';
+import LogCard, { CARD_GAP } from './LogCard';
+
+type MockCardData = {
+  id: number;
+  title: string;
+  location: string;
+  date: string;
+  backgroundImage: string;
+};
+
+const MOCK_LOG_CARD: MockCardData[] = [
+  {
+    id: 1,
+    title: '민지사진',
+    date: '2024.08.04',
+    location: '강원도',
+    backgroundImage:
+      'https://i.namu.wiki/i/8BAuDmjlFbHoGpGTyTUJyeIsrWw7vrGKTvbOBS1DbaLNHHFL6D05TSZEyVGGffn_RIs6zrf4jCb5Xq5Lnbs8QQ.webp',
+  },
+  {
+    id: 2,
+    title: '준비갈완료',
+    date: '2024.08.04',
+    location: '경상남도',
+    backgroundImage:
+      'https://t1.daumcdn.net/news/202406/27/poctan/20240627172416746baii.jpg',
+  },
+  {
+    id: 3,
+    title: '이미지 구하기 귀찮다',
+    date: '2024.08.04',
+    location: '제주도',
+    backgroundImage:
+      'https://img3.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202406/20/starfashion/20240620094035441whlh.jpg',
+  },
+];
+
+export default function LogCardList() {
+  return (
+    <View
+      className="mt-12 flex flex-row flex-wrap "
+      style={{
+        gap: CARD_GAP,
+      }}
+    >
+      {MOCK_LOG_CARD.map((data) => (
+        <View key={data.title + data.backgroundImage}>
+          <LogCard
+            title={data.title}
+            date={data.date}
+            location={data.location}
+            backgroundImage={data.backgroundImage}
+          />
+        </View>
+      ))}
+    </View>
+  );
+}
