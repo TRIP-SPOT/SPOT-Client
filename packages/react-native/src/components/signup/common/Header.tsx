ed05c0d008c703725eb3ab6b10e60570b53bd368
d0422 <rlfehd2013@naver.com>
2024-08-04 16:26:44 +0900

    refactor: 헤더 컴포넌트 공통으로 분리 및 Signup 헤더 적용

--- packages/react-native/src/components/signup/common/Header.tsx
+++ packages/react-native/src/components/signup/common/Header.tsx
@@ -1,22 +1,21 @@
-import { View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import Back from '@/assets/BackIcon';
 import Cancel from '@/assets/CancelIcon';
+import Header from '@/components/common/Header';
 
 interface HeaderProps {
   onBack: () => void;
   onCancel: () => void;
 }
 
-export default function Header({ onBack, onCancel }: HeaderProps) {
+export default function SignupHeader({ onBack, onCancel }: HeaderProps) {
   return (
-    <View className="w-full flex-row justify-between">
-      <TouchableOpacity onPress={onBack}>
-        <Back />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onCancel}>
-        <Cancel />
-      </TouchableOpacity>
-    </View>
+    <Header
+      onBack={onBack}
+      RightActionButton={
+        <TouchableOpacity onPress={onCancel}>
+          <Cancel />
+        </TouchableOpacity>
+      }
+    />
   );
 }
