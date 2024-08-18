b29bd3092807f1c4ef4240edd249a346f9a3c0f4
d0422 <rlfehd2013@naver.com>
2024-08-18 15:22:10 +0900

    refactor: BottomSheet 훅으로 분리

---
+++ packages/react-native/src/hooks/useBottomSheet.tsx
@@ -0,0 +1,60 @@
+import { ReactNode, useRef } from 'react';
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetBackdropProps,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
+
+interface BottomSheetProps {
+  isShow?: boolean;
+  children: ReactNode;
+}
+
+const SNAP_POINTS = ['30%'];
+
+const renderBackdropComponent = (props: BottomSheetBackdropProps) => (
+  <BottomSheetBackdrop
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...props}
+    pressBehavior="close"
+    // Backdrop이 등장하기 시작하는 bottom sheet의 index
+    appearsOnIndex={0}
+    // Backdrop이 사라지기 시작하는 bottom sheet의 index
+    disappearsOnIndex={-1}
+  />
+);
+
+export default function useBottomSheet() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function BottomSheetComponent({ isShow, children }: BottomSheetProps) {
+    if (!isShow) {
+      return null;
+    }
+
+    return (
+      <BottomSheet
+        backdropComponent={renderBackdropComponent}
+        ref={bottomSheetRef}
+        snapPoints={SNAP_POINTS}
+        enablePanDownToClose
+      >
+        <BottomSheetView>{children}</BottomSheetView>
+      </BottomSheet>
+    );
+  }
+
+  const hideBottomSheet = () => {
+    bottomSheetRef.current?.close();
+  };
+
+  const showBottonSheet = () => {
+    bottomSheetRef.current?.snapToIndex(0);
+  };
+
+  return {
+    BottomSheet: BottomSheetComponent,
+    showBottonSheet,
+    hideBottomSheet,
+  };
+}
