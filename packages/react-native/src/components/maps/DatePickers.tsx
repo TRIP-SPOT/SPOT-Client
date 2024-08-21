6ce8f7c63327d263ff60949f4574560ed0f70fbf
d0422 <rlfehd2013@naver.com>
2024-08-21 22:40:27 +0900

    refactor: datePcikers 추가

---
+++ packages/react-native/src/components/maps/DatePickers.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import { TouchableOpacity, View } from 'react-native';
+import DatePicker from 'react-native-date-picker';
+import { Font } from 'design-system';
+
+const getDisplayDate = (displayDate: Date) => {
+  return `${displayDate.getFullYear()}-${displayDate.getMonth() + 1}-${displayDate.getDate()}`;
+};
+
+export default function DatePickers() {
+  const [date, setDate] = useState({
+    start: new Date(),
+    end: new Date(),
+  });
+
+  const [selectionMode, setSelectionMode] = useState<'start' | 'end'>();
+
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <View className="justify-evenly flex-row flex-1">
+        <View>
+          <Font color="black" type="ui-text">
+            From
+          </Font>
+          <TouchableOpacity
+            onPress={() => {
+              setOpen(true);
+              setSelectionMode('start');
+            }}
+          >
+            <Font color="black" type="body1">
+              {getDisplayDate(date.start)}
+            </Font>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Font color="black" type="ui-text">
+            To
+          </Font>
+          <TouchableOpacity
+            onPress={() => {
+              setOpen(true);
+              setSelectionMode('end');
+            }}
+          >
+            <Font color="black" type="body1">
+              {getDisplayDate(date.end)}
+            </Font>
+          </TouchableOpacity>
+        </View>
+      </View>
+      {selectionMode && (
+        <DatePicker
+          modal
+          open={open}
+          date={date[selectionMode]}
+          mode="date"
+          onConfirm={(selectedDate) => {
+            setOpen(false);
+            setDate((prev) => ({
+              ...prev,
+              [selectionMode]: selectedDate,
+            }));
+          }}
+          onCancel={() => {
+            setOpen(false);
+          }}
+        />
+      )}
+    </>
+  );
+}
