dd155f3b4151b1fe05ddb11e60ee19122261311d
d0422 <rlfehd2013@naver.com>
2024-08-31 13:06:05 +0900

    feat: multiline,numberOfLines props 추가

--- packages/design-system/src/TextField.tsx
+++ packages/design-system/src/TextField.tsx
@@ -1,15 +1,25 @@
 import { useState } from 'react';
-import { TextInput } from 'react-native';
+import { Platform, TextInput } from 'react-native';
 
 interface TextField {
   value: string;
   onChange: (value: string) => void;
   onSubmit?: () => void;
   placeholder: string;
+  multiline?: boolean;
   isCorrect?: boolean;
+  numberOfLines?: number;
 }
 
-export function TextField({ value, onChange, placeholder, onSubmit, isCorrect }: TextField) {
+export function TextField({
+  value,
+  onChange,
+  placeholder,
+  onSubmit,
+  isCorrect,
+  multiline,
+  numberOfLines,
+}: TextField) {
   const defaultClassName =
     'text-SPOT-black text-body2 rounded-md p-4 bg-SPOT-white/60 border-[2px] border-bg-SPOT-white/60';
   const incorrectClassName = 'border-SPOT-red border-[2px]';
@@ -22,15 +32,23 @@ export function TextField({ value, onChange, placeholder, onSubmit, isCorrect }:
 
     return 'border-none';
   };
-
+  console.log(numberOfLines, Platform.OS);
   return (
     <TextInput
+      multiline={multiline}
       value={value}
       onChangeText={onChange}
       placeholder={placeholder}
-      placeholderTextColor='#ffffff'
+      placeholderTextColor="#ffffff"
       className={`${defaultClassName} ${getBorderClassName()}`}
       onSubmitEditing={onSubmit}
+      numberOfLines={Platform.OS === 'ios' ? undefined : numberOfLines}
+      style={{
+        minHeight:
+          Platform.OS === 'ios' && numberOfLines && numberOfLines
+            ? 30 * numberOfLines
+            : undefined,
+      }}
     />
   );
 }
