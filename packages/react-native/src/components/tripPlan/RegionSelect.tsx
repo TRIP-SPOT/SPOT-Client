e5447b9107d327466fbaaa54c06a60c10dd24978
d0422 <rlfehd2013@naver.com>
2024-09-12 00:38:19 +0900

    feat: Trip Planner Post 페이지 추가

---
+++ packages/react-native/src/components/tripPlan/RegionSelect.tsx
@@ -0,0 +1,49 @@
+import { View } from 'react-native';
+import { Font } from 'design-system';
+import { Dropdown } from 'react-native-element-dropdown';
+import { Region, REGION_MAPPER } from '@/constants/CITY';
+import useTripPlanFormState from '@/hooks/useTripPlanFormState';
+import { KoreaLocationName } from '@/types/map';
+
+export type RegionSelectType = {
+  label: KoreaLocationName;
+  value: Region;
+};
+
+export default function RegionSelect() {
+  const { region, setRegion } = useTripPlanFormState();
+  return (
+    <View>
+      <Font type="body2" color="white">
+        도/특별시/광역시
+      </Font>
+      <Dropdown
+        containerStyle={{
+          borderRadius: 6,
+        }}
+        style={{
+          backgroundColor: '#4C4C4C',
+          borderRadius: 6,
+          paddingVertical: 16,
+          paddingHorizontal: 14,
+          marginTop: 8,
+        }}
+        placeholder="지역을 선택해주세요"
+        placeholderStyle={{
+          color: 'white',
+        }}
+        selectedTextStyle={{
+          color: 'white',
+        }}
+        data={Object.entries(REGION_MAPPER).map(([label, value]) => ({
+          label: label as KoreaLocationName,
+          value,
+        }))}
+        valueField="value"
+        labelField="label"
+        onChange={setRegion}
+        value={region}
+      />
+    </View>
+  );
+}
--- packages/react-native/src/pages/TripPlanner/TripPlannerPost.tsx
+++ packages/react-native/src/pages/TripPlanner/TripPlannerPost.tsx
@@ -1,5 +1,16 @@
 import { View } from 'react-native';
+import Header from '@/components/common/Header';
+import TripPlanPostForm from '@/components/tripPlan/TripPlanPostForm';
+import { TripFormProvider } from '@/hooks/useTripPlanFormState';
+import BackGroundGradient from '@/layouts/BackGroundGradient';
 
 export default function TripPlannerPost() {
-  return <View />;
+  return (
+    <BackGroundGradient withoutScroll>
+      <TripFormProvider>
+        <Header title="새 여행 등록" />
+        <TripPlanPostForm />
+      </TripFormProvider>
+    </BackGroundGradient>
+  );
 }
