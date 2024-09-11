a088424ec55a5449f60e0527b67a3ef6b0d10299
d0422 <rlfehd2013@naver.com>
2024-09-11 17:30:28 +0900

    refactor: DateSelect 공통 컴포넌트로 분리

---
+++ packages/react-native/src/components/common/DateSelect.tsx
@@ -0,0 +1,89 @@
+import { Font } from 'design-system';
+import { useState } from 'react';
+import { TouchableOpacity, View } from 'react-native';
+import DatePicker from 'react-native-date-picker';
+
+interface DateSelectProps {
+  date: {
+    start: Date;
+    end: Date;
+  };
+
+  setDate: React.Dispatch<
+    React.SetStateAction<{
+      start: Date;
+      end: Date;
+    }>
+  >;
+  selectionMode?: 'start' | 'end';
+  setSelectionMode: React.Dispatch<
+    React.SetStateAction<'start' | 'end' | undefined>
+  >;
+}
+
+const getDisplayDate = (displayDate: Date) => {
+  return `${displayDate.getFullYear()}-${displayDate.getMonth() + 1}-${displayDate.getDate()}`;
+};
+
+export default function DateSelect({
+  date,
+  selectionMode,
+  setDate,
+  setSelectionMode,
+}: DateSelectProps) {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <View className="justify-evenly flex-row flex-1">
+        <View>
+          <Font color="black" type="ui-text">
+            From
+          </Font>
+          <TouchableOpacity
+            onPress={() => {
+              setOpen(true);
+              setSelectionMode('start');
+            }}
+          >
+            <Font color="black" type="body1">
+              {getDisplayDate(date.start)}
+            </Font>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Font color="black" type="ui-text">
+            To
+          </Font>
+          <TouchableOpacity
+            onPress={() => {
+              setOpen(true);
+              setSelectionMode('end');
+            }}
+          >
+            <Font color="black" type="body1">
+              {getDisplayDate(date.end)}
+            </Font>
+          </TouchableOpacity>
+        </View>
+      </View>
+      {selectionMode && (
+        <DatePicker
+          modal
+          open={open}
+          date={date[selectionMode]}
+          mode="date"
+          onConfirm={(selectedDate) => {
+            setOpen(false);
+            setDate((prev) => ({
+              ...prev,
+              [selectionMode]: selectedDate,
+            }));
+          }}
+          onCancel={() => {
+            setOpen(false);
+          }}
+        />
+      )}
+    </>
+  );
+}
--- packages/react-native/src/components/maps/RecordFormDatePickers.tsx
+++ packages/react-native/src/components/maps/RecordFormDatePickers.tsx
@@ -1,69 +1,16 @@
-import { useState } from 'react';
-import { TouchableOpacity, View } from 'react-native';
-import DatePicker from 'react-native-date-picker';
-import { Font } from 'design-system';
 import useRecordFormState from '@/hooks/useRecordFormState';
-
-const getDisplayDate = (displayDate: Date) => {
-  return `${displayDate.getFullYear()}-${displayDate.getMonth() + 1}-${displayDate.getDate()}`;
-};
+import DateSelect from '../common/DateSelect';
 
 export default function RecordFormDatePickers() {
   const { date, setDate, selectionMode, setSelectionMode } =
     useRecordFormState();
-  const [open, setOpen] = useState(false);
+
   return (
-    <>
-      <View className="justify-evenly flex-row flex-1">
-        <View>
-          <Font color="black" type="ui-text">
-            From
-          </Font>
-          <TouchableOpacity
-            onPress={() => {
-              setOpen(true);
-              setSelectionMode('start');
-            }}
-          >
-            <Font color="black" type="body1">
-              {getDisplayDate(date.start)}
-            </Font>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <Font color="black" type="ui-text">
-            To
-          </Font>
-          <TouchableOpacity
-            onPress={() => {
-              setOpen(true);
-              setSelectionMode('end');
-            }}
-          >
-            <Font color="black" type="body1">
-              {getDisplayDate(date.end)}
-            </Font>
-          </TouchableOpacity>
-        </View>
-      </View>
-      {selectionMode && (
-        <DatePicker
-          modal
-          open={open}
-          date={date[selectionMode]}
-          mode="date"
-          onConfirm={(selectedDate) => {
-            setOpen(false);
-            setDate((prev) => ({
-              ...prev,
-              [selectionMode]: selectedDate,
-            }));
-          }}
-          onCancel={() => {
-            setOpen(false);
-          }}
-        />
-      )}
-    </>
+    <DateSelect
+      date={date}
+      setDate={setDate}
+      selectionMode={selectionMode}
+      setSelectionMode={setSelectionMode}
+    />
   );
 }
