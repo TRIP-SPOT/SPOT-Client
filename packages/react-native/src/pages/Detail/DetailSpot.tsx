7044793dc22300c06fafa16b722f0c3be7776168
d0422 <rlfehd2013@naver.com>
2024-09-11 17:13:27 +0900

    refactor: useArrayToggle로 코드 개선

--- packages/react-native/src/pages/Detail/DetailSpot.tsx
+++ packages/react-native/src/pages/Detail/DetailSpot.tsx
@@ -7,36 +7,28 @@ import CardSlider from '@/components/common/CardSlider';
 import useAroundSpotQuery from '@/apis/queries/detail/useAroundSpotQuery';
 import { StackRouteProps } from '@/types/navigation';
 import MySpotDetailBottomSheet from '@/components/mypage/MySpotDetailBottomSheet';
+import useArrayToggle from '@/hooks/useArrayToggle';
 
 export default function DetailSpot() {
   const route = useRoute<StackRouteProps<'Home/Detail'>>();
-  const [selectedSpots, setSelectedSpots] = useState<number[]>([]);
   const [longPressMode, setLongPressMode] = useState(false);
   const [selectedSpot, setSelectedSpot] = useState<number>();
+  const { list, toggleItem } = useArrayToggle<number>();
 
   const { id } = route.params;
 
   const { data } = useAroundSpotQuery({ id });
 
-  const toggleSelect = (spotId: number) => {
-    if (selectedSpots.includes(spotId)) {
-      return setSelectedSpots((prev) =>
-        prev.filter((prevId) => spotId !== prevId),
-      );
-    }
-    return setSelectedSpots((prev) => [...prev, spotId]);
-  };
-
   const startLongPress = (spotId: number) => {
     if (!longPressMode) {
       setLongPressMode(true);
-      toggleSelect(spotId);
+      toggleItem(spotId);
     }
   };
 
   const handleCardClick = (spotId: number) => {
     if (longPressMode) {
-      return toggleSelect(spotId);
+      return toggleItem(spotId);
     }
     return setSelectedSpot(spotId);
   };
@@ -75,7 +67,7 @@ export default function DetailSpot() {
                 <AroundCard
                   data={item}
                   isLongPressMode={longPressMode}
-                  selectedSpots={selectedSpots}
+                  selectedSpots={list}
                   onCardClick={handleCardClick}
                   startLongPress={startLongPress}
                 />
@@ -88,7 +80,7 @@ export default function DetailSpot() {
                 <AroundCard
                   data={item}
                   isLongPressMode={longPressMode}
-                  selectedSpots={selectedSpots}
+                  selectedSpots={list}
                   startLongPress={startLongPress}
                   onCardClick={handleCardClick}
                 />
