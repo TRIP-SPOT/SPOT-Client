4755f413607a4d1af44f4369edb61f9b0a49afd7
d0422 <rlfehd2013@naver.com>
2024-09-13 19:26:32 +0900

    feat: ProfileImage를 통해 없는 경우, 닉네임프로필인경우 모두 처리

--- packages/react-native/src/hooks/useProfileImage.tsx
+++ packages/react-native/src/hooks/useProfileImage.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import useGallery from './useGallery';
 import SelectProfileIcon from '@/assets/SelectProfileIcon';
+import useNicknameQuery from '@/apis/queries/useNicknameQuery';
 
 export default function useProfileImage(uri?: string) {
   const { getPhoto } = useGallery();
+  const { nickname } = useNicknameQuery();
   const [photoUri, setPhotoUri] = useState(uri || '');
 
   const getPhtoFromLibrary = async () => {
@@ -15,18 +17,43 @@ export default function useProfileImage(uri?: string) {
     setPhotoUri(photo || '');
   };
 
-  function ProfileImage() {
+  const renderProfileContent = () => {
+    if (photoUri) {
+      return (
+        <Image
+          className="w-[140px] h-[140px] rounded-full bg-SPOT-white"
+          source={{ uri: photoUri }}
+          resizeMode="contain"
+        />
+      );
+    }
+    if (nickname && nickname.colorSet) {
+      return (
+        <View
+          className="w-[140px] h-[140px] rounded-full justify-center items-center"
+          style={{
+            backgroundColor: nickname?.colorSet.bgColor,
+          }}
+        >
+          <Text
+            className="font-Pretendard-Medium text-[40px]"
+            style={{
+              color: nickname.colorSet.color,
+            }}
+          >
+            {nickname.value}
+          </Text>
+        </View>
+      );
+    }
+
+    return <SelectProfileIcon />;
+  };
+
+  function ProfileImage({ disableTouch }: { disableTouch?: boolean }) {
     return (
-      <TouchableOpacity onPress={getPhtoFromLibrary}>
-        {photoUri ? (
-          <Image
-            className="w-[140px] h-[140px] rounded-full bg-SPOT-white"
-            source={{ uri: photoUri }}
-            resizeMode="contain"
-          />
-        ) : (
-          <SelectProfileIcon />
-        )}
+      <TouchableOpacity onPress={disableTouch ? undefined : getPhtoFromLibrary}>
+        {renderProfileContent()}
       </TouchableOpacity>
     );
   }
