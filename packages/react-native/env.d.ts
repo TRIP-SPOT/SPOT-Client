955c84e3325019fbc83097476e7dcc9c1c4ef056
d0422 <rlfehd2013@naver.com>
2024-09-13 17:40:31 +0900

    feat: BASE_URL, ACCESS_TOKEN, REFRESH_TOKEN 추가

--- packages/react-native/env.d.ts
+++ packages/react-native/env.d.ts
@@ -1,3 +1,6 @@
 declare module '@env' {
   export const KAKAO_APP_KEY: string;
+  export const BASE_URL: string;
+  export const DEV_ACCESS_TOKEN: string;
+  export const DEV_REFRESH_TOKEN: string;
 }
