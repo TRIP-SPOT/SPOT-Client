f5a562591cf891f85cd1b96a7a0ea59af9323bdc
d0422 <rlfehd2013@naver.com>
2024-09-13 19:54:12 +0900

    feat: 모달 회전 애니메이션 추가

--- packages/react-native/src/components/common/MutationLoadingModal.tsx
+++ packages/react-native/src/components/common/MutationLoadingModal.tsx
@@ -1,4 +1,5 @@
-import { Modal, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Modal, View, Animated, Easing } from 'react-native';
 import { Font } from 'design-system';
 import LoadingIcon from '@/assets/LoadingIcon';
 
@@ -9,16 +10,39 @@ interface MutationLoadingModalProps {
 export default function MutationLoadingModal({
   isSubmiting,
 }: MutationLoadingModalProps) {
+  const rotateAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const startRotation = () => {
+      Animated.loop(
+        Animated.timing(rotateAnim, {
+          toValue: 1,
+          duration: 10000,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+      ).start();
+    };
+
+    startRotation();
+  }, [rotateAnim]);
+
+  const rotation = rotateAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
   return (
     <Modal visible={isSubmiting} transparent animationType="fade">
-      <View className="flex-1 justify-center text-center px-8">
-        <View className="p-4 bg-[#191919] rounded-lg">
-          <LoadingIcon />
-          <View className="mt-5">
+      <View className="flex-1 justify-center text-center px-20">
+        <View className="p-4 bg-[#191919] rounded-lg justify-center items-center py-10">
+          <Animated.View style={{ transform: [{ rotate: rotation }] }}>
+            <LoadingIcon />
+          </Animated.View>
+          <View className="mt-5 items-center">
             <Font type="body1" color="white">
               잠시만
             </Font>
-
             <Font type="body1" color="white">
               기다려주세요
             </Font>
