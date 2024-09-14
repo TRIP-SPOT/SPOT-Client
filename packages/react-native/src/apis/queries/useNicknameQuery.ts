344149b4dbf1dc91a6a8ba7774759b5edac801e7
d0422 <rlfehd2013@naver.com>
2024-09-14 11:08:15 +0900

    refactor: getProfile로 api호출 및 데이터처리 전체 로직 분리

--- packages/react-native/src/apis/queries/useNicknameQuery.ts
+++ packages/react-native/src/apis/queries/useNicknameQuery.ts
@@ -31,32 +31,30 @@ export default function useNicknameQuery() {
 
   const getProfile = async () => {
     const result = await authAxios.get<ProfileResponse>('/api/user/profile');
-    return result.data.result;
+    const profile = result.data.result;
+    AppStorage.saveData({
+      key: 'nickname',
+      value: {
+        value: profile.nickname,
+        colorSet: {
+          bgColor: profile.color,
+          color: tinycolor(profile.color).darken(25).toHexString(),
+        },
+      },
+    });
+
+    const savedNickname = await AppStorage.getData('nickname');
+
+    if (!savedNickname) {
+      throw new Error('닉네임을 불러올 수 없습니다. 다시 한번 시도해보세요');
+    }
+
+    return savedNickname;
   };
 
   const { data, refetch, isLoading, isError } = useSuspenseQuery({
     queryKey: ['nickname'],
-    queryFn: async () => {
-      const profile = await getProfile();
-      AppStorage.saveData({
-        key: 'nickname',
-        value: {
-          value: profile.nickname,
-          colorSet: {
-            bgColor: profile.color,
-            color: tinycolor(profile.color).darken(25).toHexString(),
-          },
-        },
-      });
-
-      const savedNickname = await AppStorage.getData('nickname');
-
-      if (!savedNickname) {
-        throw new Error('닉네임을 불러올 수 없습니다. 다시 한번 시도해보세요');
-      }
-
-      return savedNickname;
-    },
+    queryFn: getProfile,
   });
 
   ref.current.nickname = data;
