38658da93b3fda1e32723a7d1b6d7612c7bfc2fd
HBSPS <wiyeong79@gmail.com>
2024-09-16 22:43:05 +0900

    feat: getDateList 함수 추가 및 normalizeDate 유틸로 분리

--- packages/react-native/src/utils/date.ts
+++ packages/react-native/src/utils/date.ts
@@ -1,8 +1,45 @@
 /**
- * YYYY-MM-DD
+ * Date 객체의 모든 시간을 통일
+ * @description 정확한 시간을 비교할 경우 `new Date` 대신 사용
+ */
+export const normalizeDate = (date?: Date | string) => {
+  const normalizedDate = date ? new Date(date) : new Date();
+  normalizedDate.setUTCHours(0, 0, 0, 0);
+  return normalizedDate;
+};
+
+/**
+ * `seperator`를 기준으로 날짜 문자열 반환
+ *
+ * `default`: YYYY-MM-DD
  */
 export const getDateString = (date: Date | string, seperator: string = '-') => {
   const tmpDate = new Date(date);
 
   return `${tmpDate.getFullYear()}${seperator}${String(tmpDate.getMonth() + 1).padStart(2, '0')}${seperator}${String(tmpDate.getDate()).padStart(2, '0')}`;
+};
+
+/**
+ * 시작 Date와 끝 Date를 포함하는 날짜 배열 반환
+ * @param startDate 시작날짜
+ * @param endDate 끝날짜
+ * @returns 날짜 배열
+ */
+export const getDateList = (
+  startDate: Date | string,
+  endDate: Date | string,
+) => {
+  const dateList = [];
+
+  const normalizedStartDate = normalizeDate(startDate);
+  const normalizedEndDate = normalizeDate(endDate);
+
+  if (normalizedStartDate.getTime() > normalizedEndDate.getTime()) return [];
+
+  while (normalizedStartDate <= normalizedEndDate) {
+    dateList.push(normalizedStartDate.getDate());
+    normalizedStartDate.setDate(normalizedStartDate.getDate() + 1);
+  }
+
+  return dateList;
 };
