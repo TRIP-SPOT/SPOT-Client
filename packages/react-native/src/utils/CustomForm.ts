a87d9ea6475eee383135f68b7d5a327b28d1aee5
d0422 <rlfehd2013@naver.com>
2024-09-20 16:05:15 +0900

    feat: CustomForm class추가

---
+++ packages/react-native/src/utils/CustomForm.ts
@@ -0,0 +1,30 @@
+import { Platform } from 'react-native';
+import { Asset } from 'react-native-image-picker';
+import { getDateString, normalizeDate } from './date';
+
+class CustomForm {
+  #form: FormData;
+
+  constructor() {
+    this.#form = new FormData();
+  }
+
+  append(key: string, value: unknown) {
+    this.#form.append(key, value);
+  }
+
+  appendImage(key: string, image: Asset) {
+    this.#form.append(key, {
+      name: `${getDateString(normalizeDate())}.${image.fileName}`,
+      type: image.type,
+      uri:
+        Platform.OS === 'ios' ? image.uri?.replace('file://', '') : image.uri,
+    });
+  }
+
+  getForm() {
+    return this.#form;
+  }
+}
+
+export default CustomForm;
