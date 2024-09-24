e881d2e73905d4c23bec4199ea7e8cfbfad8ee49
HBSPS <wiyeong79@gmail.com>
2024-09-24 21:46:09 +0900

    fix: 일정에 담은 장소가 없어도 공간을 차지하도록 변경

--- packages/react-native/src/pages/TripPlanner/TripPlannerDetail.tsx
+++ packages/react-native/src/pages/TripPlanner/TripPlannerDetail.tsx
@@ -11,6 +11,7 @@ import { getDisplayRegion } from '@/utils/getDisplayRegionName';
 import AroundCard from '@/components/detail/AroundCard';
 import SpotDetailBottomSheet from '@/components/common/SpotDetailBottomSheet';
 import useTripPlanMySpotQuery from '@/apis/queries/tripPlan/useTripPlanMySpotQuery';
+import Spacing from '@/components/common/Spacing';
 
 export default withSuspense(function TripPlannerDetail() {
   const route = useRoute<StackRouteProps<'TripPlanner/Detail'>>();
@@ -35,8 +36,13 @@ export default withSuspense(function TripPlannerDetail() {
           })}
         </Font>
       </View>
-      <View className="rounded-2xl bg-SPOT-white h-full mt-5 ">
-        <ScrollView className="px-5 py-6">
+      <View className="rounded-t-2xl bg-SPOT-white flex-1 mt-5 ">
+        <Spacing height={10} />
+        <ScrollView className="px-5 flex-1 ">
+          <Font.Bold type="body1" color="black">
+            일정
+          </Font.Bold>
+          <Spacing height={10} />
           <TouchableOpacity
             onPress={() =>
               navigation.navigate('TripPlanner/EditPlan', { tripId })
@@ -45,52 +51,53 @@ export default withSuspense(function TripPlannerDetail() {
             {/* eslint-disable-next-line global-require */}
             <Image source={require('../../assets/plan.png')} />
           </TouchableOpacity>
+          <Spacing height={10} />
           <View>
-            {data.restaurant.length > 0 && (
-              <CardSlider
-                gap={8}
-                title="담은 음식점"
-                titleColor="black"
-                data={data.restaurant}
-                titleGap={4}
-                renderItem={({ item }) => (
-                  <AroundCard
-                    data={item}
-                    onCardClick={() => setSelectedSpot(item.contentId)}
-                  />
-                )}
-              />
-            )}
-            {data.attraction.length > 0 && (
-              <CardSlider
-                gap={8}
-                title="담은 관광지"
-                titleColor="black"
-                data={data.attraction}
-                titleGap={4}
-                renderItem={({ item }) => (
-                  <AroundCard
-                    data={item}
-                    onCardClick={() => setSelectedSpot(item.contentId)}
-                  />
-                )}
-              />
-            )}
-            {data.accommodation.length > 0 && (
-              <CardSlider
-                gap={8}
-                title="담은 숙소"
-                titleColor="black"
-                data={data.accommodation}
-                titleGap={4}
-                renderItem={({ item }) => (
-                  <AroundCard
-                    data={item}
-                    onCardClick={() => setSelectedSpot(item.contentId)}
-                  />
-                )}
-              />
-            )}
+            <CardSlider
+              gap={8}
+              title="담은 음식점"
+              titleColor="black"
+              data={data.restaurant}
+              titleGap={4}
+              renderItem={({ item }) => (
+                <AroundCard
+                  data={item}
+                  onCardClick={() => setSelectedSpot(item.contentId)}
+                />
+              )}
+            />
+            {data.restaurant.length === 0 && <Spacing height={160} />}
+            <Spacing height={10} />
+            <CardSlider
+              gap={8}
+              title="담은 관광지"
+              titleColor="black"
+              data={data.attraction}
+              titleGap={4}
+              renderItem={({ item }) => (
+                <AroundCard
+                  data={item}
+                  onCardClick={() => setSelectedSpot(item.contentId)}
+                />
+              )}
+            />
+            {data.attraction.length === 0 && <Spacing height={160} />}
+            <Spacing height={10} />
+            <CardSlider
+              gap={8}
+              title="담은 숙소"
+              titleColor="black"
+              data={data.accommodation}
+              titleGap={4}
+              renderItem={({ item }) => (
+                <AroundCard
+                  data={item}
+                  onCardClick={() => setSelectedSpot(item.contentId)}
+                />
+              )}
+            />
+            {data.accommodation.length === 0 && <Spacing height={160} />}
+            <Spacing height={10} />
           </View>
         </ScrollView>
       </View>
