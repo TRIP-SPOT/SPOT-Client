8479a0c50c99763ca5fb2786209c8e3687f29288
d0422 <rlfehd2013@naver.com>
2024-09-26 00:39:31 +0900

    feat: MyBadge api UI적용

--- packages/react-native/src/pages/MyPage/MyBadge.tsx
+++ packages/react-native/src/pages/MyPage/MyBadge.tsx
@@ -3,40 +3,42 @@ import { FlatList, View } from 'react-native';
 import Badge, { badgePath } from '@/components/common/Badge';
 import Spacing from '@/components/common/Spacing';
 import BadgeListBottomSheet from '@/components/mypage/BadgeListBottomSheet';
+import useMyBadgeQuery from '@/apis/queries/mypage/useMyBadgeQuery';
+import withSuspense from '@/components/HOC/withSuspense';
 
-export default function MyBadge() {
+export default withSuspense(function MyBadge() {
+  const { data: badges } = useMyBadgeQuery();
   const [containerWidth, setContainerWidth] = useState(0);
-  const [selectedBadge, setSelectedBadge] = useState<keyof typeof badgePath>();
+  // const [selectedBadge, setSelectedBadge] = useState<keyof typeof badgePath>();
 
-  const locationList = Object.keys(badgePath) as (keyof typeof badgePath)[];
   const numColumns = 3;
   const paddingHorizontal = 8;
 
   return (
     <>
       <FlatList
-        data={locationList}
+        data={badges}
         onLayout={(e) => setContainerWidth(e.nativeEvent.layout.width)}
         style={{ flex: 1, backgroundColor: 'black', paddingHorizontal }}
         renderItem={({ item, index }) => (
           <View>
             <Badge
-              location={item}
+              location={item.badgeRegion}
               width={(containerWidth - paddingHorizontal * 2) / numColumns}
-              label={item}
-              onPress={() => setSelectedBadge(item)}
-              count={index % 4}
+              label={item.badgeRegion}
+              // onPress={() => setSelectedBadge(item.badgeRegion)}
+              count={item.count}
             />
             <Spacing height={20} />
           </View>
         )}
-        keyExtractor={(item) => item}
+        keyExtractor={(item) => item.badgeRegion}
         numColumns={numColumns}
       />
-      <BadgeListBottomSheet
+      {/* <BadgeListBottomSheet
         selectedBadge={selectedBadge}
         onClose={() => setSelectedBadge(undefined)}
-      />
+      /> */}
     </>
   );
-}
+});
