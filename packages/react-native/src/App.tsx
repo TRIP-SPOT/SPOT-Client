af97ffe77d35a658c977e8920f6221b9c29b3f3f
d0422 <rlfehd2013@naver.com>
2024-09-26 15:52:42 +0900

    feat: 오류 발생시 sentry로 보내도록 처리

--- packages/react-native/src/App.tsx
+++ packages/react-native/src/App.tsx
@@ -4,6 +4,12 @@ import StackNavigator from '@routes/StackNavigator';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Alert } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as Sentry from '@sentry/react-native';
+import { SENTRY_DSN } from '@env';
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+});
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,13 +18,14 @@ const queryClient = new QueryClient({
       throwOnError: true,
     },
     mutations: {
-      onError: () => {
+      onError: (err) => {
+        Sentry.captureException(err);
         Alert.alert('오류가 발생했어요', '잠시뒤에 시도해보세요.');
       },
     },
   },
 });
-export default function App() {
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView style={{ flex: 1 }}>
@@ -31,3 +38,5 @@ export default function App() {
     </QueryClientProvider>
   );
 }
+
+export default Sentry.wrap(App);
--- packages/react-native/src/components/HOC/withSpotErrorBoundary.tsx
+++ packages/react-native/src/components/HOC/withSpotErrorBoundary.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import * as Sentry from '@sentry/react-native';
 import Error from '../common/Error';
 
 export default function withSpotErrorBoundary<T extends object>(
@@ -7,7 +8,10 @@ export default function withSpotErrorBoundary<T extends object>(
 ) {
   return function ErrorBoundaryComponent({ ...props }: T) {
     return (
-      <ErrorBoundary fallback={<Error />}>
+      <ErrorBoundary
+        fallback={<Error />}
+        onError={(err) => Sentry.captureException(err)}
+      >
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...props} />
       </ErrorBoundary>
