bfc9c4d5173991b4aa8cd107f1d9b345dd42ee5c
d0422 <rlfehd2013@naver.com>
2024-09-27 17:15:19 +0900

    feat: 마이페이지 좋아요 버튼 및 기능 추가

--- packages/react-native/src/components/mypage/MySpotBlock.tsx
+++ packages/react-native/src/components/mypage/MySpotBlock.tsx
@@ -1,39 +1,109 @@
-import { Font } from 'design-system';
+import { useState } from 'react';
 import { ImageBackground, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Font } from 'design-system';
+import useSpotLikeMutation from '@/apis/mutations/useSpotLikeMutation';
+import { MySpotResponse } from '@/apis/queries/mypage/useMySpotsQuery';
+import HeartIcon from '@/assets/HeartIcon';
+import { StackNavigation } from '@/types/navigation';
+import { getDisplayRegion } from '@/utils/getDisplayRegionName';
+import MutationLoadingModal from '../common/MutationLoadingModal';
 
 interface MySpotBlockProps {
-  title: string;
-  location?: string;
-  backgroundImage: string;
+  mySpot: MySpotResponse;
   width: number;
   gap: number;
-  handleClickBlock: () => void;
+}
+interface CardLike {
+  likeCount: number;
+  isLiked: boolean;
 }
 
-export default function MySpotBlock({
-  title,
-  location,
-  backgroundImage,
-  width,
-  gap,
-  handleClickBlock,
-}: MySpotBlockProps) {
+export default function MySpotBlock({ mySpot, width, gap }: MySpotBlockProps) {
+  const navigation = useNavigation<StackNavigation<'MyPage/Profile'>>();
+  const {
+    posterUrl,
+    name,
+    region,
+    city,
+    likeCount,
+    isLiked,
+    id,
+    contentId,
+    workId,
+  } = mySpot;
+
+  const [cardLike, setCardLike] = useState<CardLike>({
+    isLiked,
+    likeCount,
+  });
+  const { like, isLikePending, isCancelLikePending, cancelLike } =
+    useSpotLikeMutation({ contentId });
+
+  const handleClickLike = () => {
+    if (cardLike.isLiked) {
+      setCardLike((prev) => ({
+        isLiked: !prev.isLiked,
+        likeCount: prev.likeCount - 1,
+      }));
+      cancelLike({ id });
+      return;
+    }
+
+    setCardLike((prev) => ({
+      isLiked: !prev.isLiked,
+      likeCount: prev.likeCount + 1,
+    }));
+    like({ id });
+  };
+
+  const handleClick = () => {
+    navigation.navigate('MyPage/Detail', {
+      contentId,
+      id,
+      workId,
+    });
+  };
+
   return (
     <ImageBackground
-      source={{ uri: backgroundImage }}
+      source={{ uri: posterUrl }}
       style={{ width, height: width * 1.3, margin: gap / 2 }}
       className="bg-red-300 rounded-lg overflow-hidden"
     >
+      <MutationLoadingModal
+        isSubmiting={isLikePending || isCancelLikePending}
+      />
       <TouchableOpacity
         className="flex-1 justify-end bg-black/40"
-        onPress={handleClickBlock}
+        onPress={handleClick}
       >
+        <View className="flex-row justify-end items-center">
+          <TouchableOpacity
+            className="flex-row items-center p-2"
+            onPress={handleClickLike}
+          >
+            <HeartIcon
+              width={15}
+              height={15}
+              color={cardLike.isLiked ? 'red' : 'white'}
+            />
+            <View className="ml-1">
+              <Font type="body3" color="white">
+                {cardLike.likeCount}
+              </Font>
+            </View>
+          </TouchableOpacity>
+        </View>
         <View className="p-2 bg-SPOT-black">
           <Font.Bold type="body1" color="white">
-            {title}
+            {name}
           </Font.Bold>
           <Font type="body3" color="white" opacity={0.7}>
-            {location}
+            {getDisplayRegion({
+              locationEnum: region,
+              cityEnum: city,
+            })}
           </Font>
         </View>
       </TouchableOpacity>
--- packages/react-native/src/pages/MyPage/MySpot.tsx
+++ packages/react-native/src/pages/MyPage/MySpot.tsx
@@ -1,18 +1,14 @@
 import { Dimensions, FlatList, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 import { Font } from 'design-system';
 import MySpotBlock from '@/components/mypage/MySpotBlock';
 import withSuspense from '@/components/HOC/withSuspense';
 import useMySpotsQuery from '@/apis/queries/mypage/useMySpotsQuery';
-import { getDisplayRegion } from '@/utils/getDisplayRegionName';
-import { StackNavigation } from '@/types/navigation';
 
 const { width } = Dimensions.get('window');
 
 export default withSuspense(
   function MySpot() {
     const { data: mySpots } = useMySpotsQuery();
-    const navigation = useNavigation<StackNavigation<'MyPage/Profile'>>();
     const numColumns = 2;
     const paddingHorizontal = 8;
     const gap = 16;
@@ -33,21 +29,9 @@ export default withSuspense(
         style={{ flex: 1, backgroundColor: 'black', paddingHorizontal }}
         renderItem={({ item }) => (
           <MySpotBlock
-            title={item.name}
-            backgroundImage={item.posterUrl}
-            location={getDisplayRegion({
-              locationEnum: item.region,
-              cityEnum: item.city,
-            })}
+            mySpot={item}
             width={(width - gap * 2 - paddingHorizontal * 2) / numColumns}
             gap={gap}
-            handleClickBlock={() =>
-              navigation.navigate('MyPage/Detail', {
-                contentId: item.contentId,
-                id: item.id,
-                workId: item.workId,
-              })
-            }
           />
         )}
         keyExtractor={(item) => item.name + item.id + item.contentId}
