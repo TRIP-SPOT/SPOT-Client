81ee7c69c966ac7effd390f4aeb34219405c43ed
HBSPS <wiyeong79@gmail.com>
2024-09-27 22:48:22 +0900

    fix: 프로필 이미지 닉네임으로 설정하는 경우 최대 3글자 까지만 출력되도록 변경

--- packages/react-native/src/hooks/useProfileImage.tsx
+++ packages/react-native/src/hooks/useProfileImage.tsx
@@ -43,7 +43,7 @@ export default function useProfileImage() {
               color: profile.colorSet.color,
             }}
           >
-            {profile.nickname}
+            {profile.nickname.slice(0, 3)}
           </Text>
         </View>
       );
