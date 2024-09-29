1559f494b35e8b1f754b70fc5a7ae0199e6c3ca6
d0422 <rlfehd2013@naver.com>
2024-09-29 22:08:27 +0900

    chore; EmptyPlan 문구 추가

--- packages/react-native/src/components/tripPlan/EmptyPlan.tsx
+++ packages/react-native/src/components/tripPlan/EmptyPlan.tsx
@@ -10,6 +10,9 @@ export default function EmptyPlan() {
       <Font type="body1" color="white">
         나만의 여행을 채워보세요
       </Font>
+      <Font type="body1" color="white">
+        나의 여행은 Trip Planner탭에서 확인할 수 있어요
+      </Font>
     </View>
   );
 }
