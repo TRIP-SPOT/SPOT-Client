1d62eb0f30989f9e4c7140f64886c3ff1f591982
d0422 <rlfehd2013@naver.com>
2024-09-30 15:18:41 +0900

    chore: MAPPER객체, REVERSE MAPPER객체 추가

--- packages/react-native/src/constants/BADGE_ACQUISITION.ts
+++ packages/react-native/src/constants/BADGE_ACQUISITION.ts
@@ -4,3 +4,9 @@ export enum BadgeAcquisition {
   BY_CAMERA_FILTER,
   BY_RECORD,
 }
+
+export const ACQUISITION_MAPPER: Record<BadgeAcquisition, string> = {
+  [BadgeAcquisition.BY_QUIZ]: 'Spot! 퀴즈 정답',
+  [BadgeAcquisition.BY_CAMERA_FILTER]: 'Spot! 카메라 필터',
+  [BadgeAcquisition.BY_RECORD]: '소중한 여행 기록 작성',
+};
