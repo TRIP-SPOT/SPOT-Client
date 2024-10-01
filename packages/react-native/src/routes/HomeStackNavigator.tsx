2d3e4f50d29f917c5bed95c422df8ff8e320ba7c
d0422 <rlfehd2013@naver.com>
2024-10-02 08:51:34 +0900

    chore: Home 관련 페이지 Home디렉토리로 이동

--- packages/react-native/src/routes/HomeStackNavigator.tsx
+++ packages/react-native/src/routes/HomeStackNavigator.tsx
@@ -1,9 +1,9 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from '@/pages/Home';
+import Home from '@/pages/Home/Home';
 import { StackParamList } from '@/types/navigation';
 import Search from '@/pages/Search';
-import Detail from '@/pages/Detail';
-import HomeSpotAdd from '@/pages/HomeSpotAdd';
+import Detail from '@/pages/Home/Detail';
+import HomeSpotAdd from '@/pages/Home/HomeSpotAdd';
 import TripPlannerPost from '@/pages/TripPlanner/TripPlannerPost';
 
 const Stack = createStackNavigator<StackParamList>();
